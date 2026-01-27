961fb2cb3a78a1c13a9d8b8758fcd7b6caaf9ee4
Robert Johnson <rboswellj@gmail.com>
2026-01-27 00:20:40 -0500

    Declared function prototypes and moved all functions below main for clarity. Also fixed the output on Moudlus so it is no longer outputting doubles. An Integer output made more sense.

--- calculator.c
+++ calculator.c
@@ -3,7 +3,7 @@ Robert Johnson
 CSE-130-50-4262
 1/26/2026
 Calculator
-A Calculator program that can perform addition, subtraction, multiplication and division, 
+A Calculator program that can perform addition, subtraction, multiplication and division,
 as well as modulus and prime tests.
 Only needs to be able to perform on 2 variables.
 
@@ -15,13 +15,60 @@ Figure out how to confirm input in a double for other functions
 #include <stdio.h>
 #include <stdlib.h>
 
+// Function prototypes
+int mainMenu();
+double collectFirstDouble();
+double collectSecondDouble();
+int testPrime(int n);
+void printHeader(char *operation);
+void printOutput(double result);
+void callOperation(int operation);
+
+int main()
+{
+    int notExit = 1;
+    char continueChoice;
+
+    do
+    {
+        // set the operation value by calling the mainMenu function
+        int operationSelection = mainMenu();
+
+        // Calls switch function that launches the proper operation
+        callOperation(operationSelection);
+        printf("\n");
+
+        // Ask user if they want to continue or exit
+        printf("Would you like to perform another operation? (<y/n>): ");
+        scanf(" %c", &continueChoice);
+        if (continueChoice == 'n' || continueChoice == 'N')
+        {
+            notExit = 0;
+            printf("Exiting calculator. Goodbye!\n");
+            break;
+        }
+        else if (continueChoice != 'y' && continueChoice != 'Y')
+        {
+            printf("Invalid choice. Exiting calculator. Goodbye!\n");
+            break;
+        }
+        printf("\n");
+        printf("====================================\n\n");
+        printf("Returning to main menu...\n\n");
+
+    } while (notExit);
+
+    return 0;
+}
 
 // Function to display the main menu and get user selection
-int mainMenu(){
+int mainMenu()
+{
     int selection;
     int valid = 0;
     // Runs through loop once and then checks for valid input
-    do {
+    do
+    {
         printf("\n");
         printf("Calculator Menu:\n\n");
         printf("\t(1) Addition\n");
@@ -37,16 +84,19 @@ int mainMenu(){
         if (selection < 1 || selection > 7)
         {
             printf("\nInvalid selection. Please select a valid operation\n");
-        } else {
-            //if selection is valid flag to truthy value
+        }
+        else
+        {
+            // if selection is valid flag to truthy value
             valid = 1;
         }
     } while (!valid);
-    return selection;    
+    return selection;
 }
 
 // Used to prompt for first double input on all operations except modulus and prime test
-double collectFirstDouble() {
+double collectFirstDouble()
+{
     double a;
     printf("Enter first number: ");
     scanf("%lf", &a);
@@ -54,7 +104,8 @@ double collectFirstDouble() {
 }
 
 // Used to prompt for second double input on all operations except modulus and prime test
-double collectSecondDouble() {
+double collectSecondDouble()
+{
     double b;
     printf("Enter second number: ");
     scanf("%lf", &b);
@@ -62,18 +113,25 @@ double collectSecondDouble() {
 }
 
 // Function to test if a number is prime
-int testPrime(int n) {
-    if (n <= 1) return 0;         // numbers less than or equal to 1 are not prime
-    if (n == 2) return 1;        // 2 is prime
-    if (n % 2 == 0) return 0;    // even numbers > 2 are not prime
-    for (int i = 3; i * i <= n; i += 2) {  // only check odd numbers. Check up to sqrt(n)
-        if (n % i == 0) return 0;   // found a divisor, not prime
+int testPrime(int n)
+{
+    if (n <= 1)
+        return 0; // numbers less than or equal to 1 are not prime
+    if (n == 2)
+        return 1; // 2 is prime
+    if (n % 2 == 0)
+        return 0; // even numbers > 2 are not prime
+    for (int i = 3; i * i <= n; i += 2)
+    { // only check odd numbers. Check up to sqrt(n)
+        if (n % i == 0)
+            return 0; // found a divisor, not prime
     }
     return 1; // no divisors found, is prime
 }
 
 // Function to print operation header formatting
-void printHeader(char* operation[]) {
+void printHeader(char *operation)
+{
     printf("\n");
     printf("====================================\n\n");
     printf("You selected %s\n\n", operation);
@@ -82,7 +140,8 @@ void printHeader(char* operation[]) {
 }
 
 // Function to print output formatting
-void printOutput(double result) {
+void printOutput(double result)
+{
     printf("\n");
     printf("====================================\n\n");
     printf("Result: %lf\n\n", result);
@@ -91,126 +150,107 @@ void printOutput(double result) {
 }
 
 // Function to call the appropriate operation based on user selection
-void callOperation(int operation) {
+void callOperation(int operation)
+{
     double a, b;
-    int c,d;
-    switch (operation) {
-        case 1:
-            // Addition
-            printHeader("Addition");
-            a = collectFirstDouble();
-            b = collectSecondDouble();
-            printOutput(a + b);
-            break;
-        case 2:
-            // Subtraction
-            printHeader("Subtraction");
-            a = collectFirstDouble();
-            b = collectSecondDouble();
-            printOutput(a - b);
-            break;
-        case 3:
-            // Multiplication
-            printHeader("Multiplication");
-            a = collectFirstDouble();
-            b = collectSecondDouble();
-            printOutput(a * b);
-            break;
-        case 4:
-            // Division
-            printHeader("Division");
-            a = collectFirstDouble();
-            b = collectSecondDouble();
-            if (b == 0) {
-                printf("\n");
-                printf("====================================\n");
-                printf("Error: Division by zero is not allowed.\n");
-                printf("====================================\n");
-                printf("\n");
-            } else {
-                printOutput(a / b);
-            }
-            break;
-        case 5:
-            // Modulus
-            printHeader("Modulus (Whole Numbers Only)");
-            printf("Enter first integer: ");
-            scanf("%d", &c);
-            printf("\nEnter second integer: ");
-            scanf("%d", &d);
-            if (d == 0) {
-                printf("\n");
-                printf("====================================\n");
-                printf("Error: Division by zero is not allowed.\n");
-                printf("====================================\n");
-                printf("\n");
-                break;
-            } else {
-                printOutput(c % d);
-            }
-            break;
-        case 6:
-            printHeader("Prime Test (Whole Numbers Only)");
-            printf("Enter an integer to test for primality: ");
-            scanf("%d", &c);
-            if (testPrime(c)) {
-                printf("\n");
-                printf("====================================\n");
-                printf("%d is prime.\n", c);
-                printf("====================================\n\n");
-                printf("\n");
-            } else {
-                printf("\n");
-                printf("====================================\n");
-                printf("%d is not prime.\n", c);
-                printf("====================================\n\n");
-                printf("\n");
-            }
-            break;
-        case 7:
-            // Exit
+    int c, d;
+    switch (operation)
+    {
+    case 1:
+        // Addition
+        printHeader("Addition");
+        a = collectFirstDouble();
+        b = collectSecondDouble();
+        printOutput(a + b);
+        break;
+    case 2:
+        // Subtraction
+        printHeader("Subtraction");
+        a = collectFirstDouble();
+        b = collectSecondDouble();
+        printOutput(a - b);
+        break;
+    case 3:
+        // Multiplication
+        printHeader("Multiplication");
+        a = collectFirstDouble();
+        b = collectSecondDouble();
+        printOutput(a * b);
+        break;
+    case 4:
+        // Division
+        printHeader("Division");
+        a = collectFirstDouble();
+        b = collectSecondDouble();
+        if (b == 0)
+        {
             printf("\n");
-            printf("====================================\n\n");
-            printf("You selected Prime Test\n\n");
-            printf("====================================\n\n");
-            printf("Exiting calculator. Goodbye!\n");
-            exit(0);
-        default:
-            printf("Invalid operation selected.\n");
-            break;
-    }
-}
-
-int main() {
-    int notExit = 1;
-    char continueChoice;
-
-    do {
-        // set the operation value by calling the mainMenu function
-        int operationSelection = mainMenu();
-
-        // Calls switch function that launches the proper operation
-        callOperation(operationSelection);
-        printf("\n");
-
-        // Ask user if they want to continue or exit
-        printf("Would you like to perform another operation? (<y/n>): ");
-        scanf(" %c", &continueChoice);
-        if (continueChoice == 'n' || continueChoice == 'N') {
-            notExit = 0;
-            printf("Exiting calculator. Goodbye!\n");
-            break;
-        } else if (continueChoice != 'y' && continueChoice != 'Y') {
-            printf("Invalid choice. Exiting calculator. Goodbye!\n");
+            printf("====================================\n");
+            printf("Error: Division by zero is not allowed.\n");
+            printf("====================================\n");
+            printf("\n");
+        }
+        else
+        {
+            printOutput(a / b);
+        }
+        break;
+    case 5:
+        // Modulus
+        printHeader("Modulus (Whole Numbers Only)");
+        printf("Enter first integer: ");
+        scanf("%d", &c);
+        printf("\nEnter second integer: ");
+        scanf("%d", &d);
+        if (d == 0)
+        {
+            printf("\n");
+            printf("====================================\n");
+            printf("Error: Division by zero is not allowed.\n");
+            printf("====================================\n");
+            printf("\n");
             break;
         }
+        else
+        {
+            printf("\n");
+            printf("====================================\n\n");
+            printf("Result: %d\n\n", c % d);
+            printf("====================================\n\n");
+            printf("\n");
+        }
+        break;
+    case 6:
+        printHeader("Prime Test (Whole Numbers Only)");
+        printf("Enter an integer to test for primality: ");
+        scanf("%d", &c);
+        if (testPrime(c))
+        {
+            printf("\n");
+            printf("====================================\n");
+            printf("%d is prime.\n", c);
+            printf("====================================\n\n");
+            printf("\n");
+        }
+        else
+        {
+            printf("\n");
+            printf("====================================\n");
+            printf("%d is not prime.\n", c);
+            printf("====================================\n\n");
+            printf("\n");
+        }
+        break;
+    case 7:
+        // Exit
         printf("\n");
         printf("====================================\n\n");
-        printf("Returning to main menu...\n\n");
-        
-    } while (notExit);
-    
-
-    return 0;
+        printf("You selected Exit\n\n");
+        printf("====================================\n\n");
+        printf("Exiting calculator. Goodbye!\n");
+        exit(0);
+    default:
+        printf("Invalid operation selected.\n");
+        break;
+    }
 }
-
